452d35e626a483104ef37eae1ccec097ef688122
Jan Piotrowski <piotrowski@gmail.com>
2017-04-04 13:10:07 +0200

    Initial Commit

---
+++ Swift Today Extension Webview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Swift Today Extension Webview
+//
+//  Created by Jan Piotrowski on 4/4/17.
+//  Copyright © 2017 Jan Piotrowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
